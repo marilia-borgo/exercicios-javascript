8e9f1d7a178fd46121e345beb1cc8cb6baa81027
marilia-borgo <marilia.bm01@gmail.com>
2022-08-12 11:24:55 -0300

    ultimo exercicio desafio

--- lista-02/js09-seis-por-dois.js
+++ lista-02/js09-seis-por-dois.js
@@ -8,7 +8,14 @@
  * 
  */
 
-// Entrada
-// Pŕoxima volta
+var diaDoMes = new Date().getDate();
+var mes = new Date().getUTCMonth();
+console.log(diaDoMes, mes)
+i = 1
 
-// Saída com base na próxima folga, calcular dia de trabalho/folga
+while (i < 90){
+    diaDoMes = diaDoMes + 6
+    console.log(diaDoMes+1, diaDoMes+2)
+    diaDoMes = diaDoMes + 2
+    i = i + 8
+}
